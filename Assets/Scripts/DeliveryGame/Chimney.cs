eb635bbff47ea856ecdc65e1a193569708337566
MisungKims <misung0212@naver.com>
2022-05-18 17:13:59 +0900

    Update Scripts

--- Assets/Scripts/DeliveryGame/Chimney.cs
+++ Assets/Scripts/DeliveryGame/Chimney.cs
@@ -1,3 +1,9 @@
+/**
+ * @brief 선물 전달 게임의 굴뚝
+ * @author 김미성
+ * @date 22-05-18
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +11,34 @@ using UnityEngine.UI;
 
 public class Chimney : Obstacle
 {
+    #region 변수
     public Image giftImage;
     public Gift gift;
+
+    private DeliveryGameManager deliveryGameManager;
+    private Inventory inventory;
+    #endregion
+
+    #region 유니티 함수
+    private void Awake()
+    {
+        deliveryGameManager = DeliveryGameManager.Instance;
+        inventory = Inventory.Instance;
+    }
 
     protected override void OnEnable()
     {
         base.OnEnable();
 
-        gift = GiftManager.Instance.RandomGift();
-        giftImage.sprite = gift.giftImage;
+        gift = inventory.RandomGet();      // 인벤토리에 있는 선물을 가져옴
+        if (gift == null)
+        {
+            deliveryGameManager.End(true);
+        }
+        else
+        {
+            giftImage.sprite = gift.giftImage;
+        }
     }
+    #endregion
 }
